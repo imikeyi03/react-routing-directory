7883b1b2ac4bf68681b9fc5cc8ad027c3bc994e7
Mikey Mann <mikey.mannx3@gmail.com>
2021-05-11 19:54:49 -0700

    working on routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { 
+  BrowserRouter,
+  Route
+ } from 'react-router-dom';
+
+//  App Components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  // Wrap the app in a BrowserRouter Tag
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      {/* Place route components here */}
+      {/* Exact path tells react to only go to the home route if its exactly the root and not ANYTHING with '/' in the url */}
+      <Route exact path="/" component={ Home } />
+      <Route path="/about" render={ () => <About title="About me"/> } />
+      <Route path="/teachers" component={ Teachers } />
+      <Route path="/courses" component={ Courses } />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
